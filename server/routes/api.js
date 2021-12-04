dc1a18c91f7ba9e9c27826c27f13abbfc3519073
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-12-04 09:47:55 -0800

    changed async awaits to promises

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,16 +4,21 @@ const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId
 const Message = require('../models/messages')
 const mongoose = require('mongoose');
-
+const User = require('../models/users')
 router.get("/api/users", (req, res) => {
-  let db_connect = dbo.getDb("speakEasy")
-  db_connect
-    .collection("users")
+  User
     .find({})
-    .toArray( (err, result) => {
-      if(err) throw err;
-      res.json(result);
-    })
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(400).json('An error has occurred'))
+
+  // let db_connect = dbo.getDb("speakEasy")
+  // db_connect
+  //   .collection("users")
+  //   .find({})
+  //   .toArray( (err, result) => {
+  //     if(err) throw err;
+  //     res.json(result);
+  //   })
 })
 
 router.post("/api/sendMessage", (req, res) => {
@@ -22,9 +27,15 @@ router.post("/api/sendMessage", (req, res) => {
   .catch(err => res.status(400).json('Something went wrong'));
 })
 
-router.get('/api/sendMessage', async (req, res) => {
-  const messages = await Message.find({})
-  res.status(200).json(messages);
+router.get('/api/sendMessage', (req, res) => {
+  Message
+    .find({})
+    .then(messages => {
+      res.status(200).json(messages)
+    })
+    .catch(err => res.status(400).json('An error has occurred'))
+
+
 
 })
 
