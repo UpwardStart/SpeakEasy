87435dded15d82a94125b170c6aa13ce2353e872
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-12-07 05:27:10 +0000

    user can sign-up

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const argon2 = require('argon2')
 const router = express.Router();
 const ObjectId = require("mongodb").ObjectId
 const Message = require('../models/messages')
@@ -23,9 +24,23 @@ router.get('/api/message', (req, res) => {
     .find({})
     .then(messages => res.status(200).json(messages))
     .catch(err => res.status(400).json('An error has occurred'))
-
-
-
 })
+
+router.post('/api/auth/sign-up', (req, res) => {
+  const {username, password} = req.body
+  if(!username || !password) {
+    res.status(400).json('Username and password are required')
+  }
+  argon2
+    .hash(password)
+    .then(hashedPassword => {
+      User
+        .create({username, password: hashedPassword})
+        .then(user => res.status(201).json(user))
+        .catch(err => res.status(401).json('Something went wrong'))
+    })
+})
+
+
 
 module.exports = router;
